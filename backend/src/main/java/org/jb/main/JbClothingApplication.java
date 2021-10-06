eaacd2192463570444bb3c04d4dcb98ba481fe12
Pragati-36 <68469090+Pragati-36@users.noreply.github.com>
2021-10-06 17:48:42 +0530

    Payment Service

---
+++ backend/src/main/java/org/jb/main/JbClothingApplication.java
@@ -0,0 +1,29 @@
+package org.jb.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class JbClothingApplication {
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbClothingApplication.class,args);
+	}
+	
+	
+	
+
+}
